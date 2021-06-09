074db7fc9f7af58ef16f3d768aa7b307c98abb4f
debrouchovea <aymar@hotmail.be>
2021-06-09 21:29:27 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -26,12 +26,12 @@ class GoNet(nn.Module):
     def __init__(self):
         super(GoNet, self).__init__()
         #caffenet = models.alexnet(pretrained=True)
-        caffenet = models.vgg16(pretrained=True)
+        #caffenet = models.vgg16(pretrained=True)
         #caffenet = models.shufflenet_v2_x0_5(pretrained=True) #ouput 1024*7*7
         #caffenet = models.mobilenet_v2(pretrained=True) #output maybe 7*7*1280
         #caffenet = models.mnasnet0_5(pretrained = True) #output should be 7*7*1280
         #caffenet = models.resnet18(pretrained = True) #512*2
-        #caffenet = models.resnet50(pretrained = True)
+        caffenet = models.resnet50(pretrained = True)
         self.convnet = nn.Sequential(*list(caffenet.children())[:-1])
         for param in self.convnet.parameters():
             param.requires_grad = False
@@ -42,9 +42,9 @@ class GoNet(nn.Module):
             param.requires_grad = True 
         """
         self.classifier = nn.Sequential(
-                #nn.Linear(256*6*6*2, 4096),
+                nn.Linear(256*6*6*2, 4096),
                 #nn.Linear(256*6*6*2, 4096), #
-                nn.Linear(512*7*7*2, 4096), #
+                #nn.Linear(512*7*7*2, 4096), #
                 #nn.Linear(1280*7*7*2, 4096), #mobilenet
                 #nn.Linear(7*7*1280*2,4096), #mnasnet
                 nn.ReLU(inplace=True),
@@ -108,9 +108,9 @@ class GoNet(nn.Module):
         #print(x.shape)
         x1 = self.convnet(x)
         #print(x1.shape)
-        x1 = x1.view(x.size(0), 512*7*7)  #7*7*1280)#1280*7*7) #512) #256*6*6)
+        x1 = x1.view(x.size(0),6*6*256 ) #512*7*7)  #7*7*1280)#1280*7*7) #512) #256*6*6)
         x2 = self.convnet(y)
-        x2 = x2.view(x.size(0), 512*7*7)  #1280*7*7) #256*6*6)
+        x2 = x2.view(x.size(0), 6*6*256) #512*7*7)  #1280*7*7) #256*6*6)
 
         
         x = torch.cat((x1, x2), 1)
