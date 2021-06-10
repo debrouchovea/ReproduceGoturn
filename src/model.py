15ebf3a5790ae8cd1a5372a335a4ab05a06535ce
debrouchovea <aymar@hotmail.be>
2021-06-10 09:10:09 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -72,7 +72,7 @@ class GoNet(nn.Module):
                 #nn.Linear(512*7*7*2, 4096), #
                 #nn.Linear(1280*7*7*2, 4096), #mobilenet
                 #nn.Linear(7*7*1280*2,4096), #mnasnet
-                #nn.Linear(1024, 4096), #resnet50
+                nn.Linear(4096, 4096), 
                 nn.ReLU(inplace=True),
                 nn.Dropout(),
                 nn.Linear(4096, 4096),
